cb4d63bbffa2fab1e934020c641f67887d842fd7
yht1995 <yaoht13@mails.tsinghua.edu.cn>
2015-01-06 20:09:22 +0800

    导入导出二进制图
    Signed-off-by: yht1995 <yaoht13@mails.tsinghua.edu.cn>

--- TravelAgency/FileIO.cs
+++ TravelAgency/FileIO.cs
@@ -6,33 +6,91 @@ using System.Threading.Tasks;
 using Microsoft.Office.Core;
 using Microsoft.Office.Interop.Excel;
 using Microsoft.Win32;
-using QuickGraph;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace TravelAgency
 {
-    using Graph = UndirectedGraph<City, TaggedUndirectedEdge<City, int>>;
-    class FileIO
+    public static class FileIO
     {
-        private string OpenExcel()
+        public static bool ImportMap(AdjacencyGraph<City,int> map)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Title = "选择文件";
-            openFileDialog.Filter = "Excel文件|*.xlsx";
+            openFileDialog.Filter = "Excel文件|*.xlsx|地图数据文件|*.map";
             openFileDialog.FileName = string.Empty;
             openFileDialog.FilterIndex = 1;
             openFileDialog.RestoreDirectory = true;
-            openFileDialog.DefaultExt = "xlsx";
+            openFileDialog.DefaultExt = "map";
             if (openFileDialog.ShowDialog() == true)
             {
-                return  openFileDialog.FileName;
+                string filename = openFileDialog.FileName;
+                if (filename.Substring(filename.LastIndexOf(".") + 1) == "xlsx")
+                {
+                    ImportFormExcel(filename, map);
+                }
+                else if (filename.Substring(filename.LastIndexOf(".") + 1) == "map")
+                {
+                    ImportFormBinMap(filename, map);
+                }
+                return true;
             }
-            return null;
+            return false;
         }
 
-        public void ImportFormExcel(Graph map,List<City> cityList)
+        public static void ExportMap(AdjacencyGraph<City, int> map)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "选择文件";
+            saveFileDialog.Filter = "地图数据文件|*.map";
+            saveFileDialog.FileName = string.Empty;
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.DefaultExt = "map";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filename = saveFileDialog.FileName;
+                Stream fStream = new FileStream(filename, FileMode.Create, FileAccess.ReadWrite);
+                BinaryFormatter binFormat = new BinaryFormatter();
+                binFormat.Serialize(fStream, map.VertexList);
+                binFormat.Serialize(fStream, map.AdjacencyMartix);
+                fStream.Close();
+            }
+        }
+
+        private static void ImportFormBinMap(string path, AdjacencyGraph<City, int> map)
         {
             map.Clear();
-            string path = OpenExcel();
+            Stream fStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            BinaryFormatter binFormat = new BinaryFormatter();
+            fStream.Position = 0;
+            map.VertexList = (List<City>)binFormat.Deserialize(fStream);
+            map.AdjacencyMartix = (List<List<int>>)binFormat.Deserialize(fStream);
+            fStream.Close();
+            foreach (City c in map.vertexList)
+            {
+                if (c.Latitude > City.latitudeMax)
+                {
+                    City.latitudeMax = c.Latitude;
+                }
+                if (c.Latitude < City.latitudeMin)
+                {
+                    City.latitudeMin = c.Latitude;
+                }
+                if (c.Longitude > City.longitudeMax)
+                {
+                    City.longitudeMax = c.Longitude;
+                }
+                if (c.Longitude < City.longitudeMin)
+                {
+                    City.longitudeMin = c.Longitude;
+                }
+            }
+        }
+
+        private static void ImportFormExcel(string path,AdjacencyGraph<City,int> map)
+        {
+            map.Clear();
             if (path == null)
             {
                 return;
@@ -42,14 +100,13 @@ namespace TravelAgency
             Worksheet mapSheet = workbook.Sheets[1];
 
             int i = 2;
-            while (mapSheet.Cells[i,1].Value2 != null)
+            while (mapSheet.Cells[i, 1].Value2 != null)
             {
-                string longitude = mapSheet.Cells[i,2].Value2;
-                string latitude = mapSheet.Cells[i,1].Value2;
+                string longitude = mapSheet.Cells[i, 2].Value2;
+                string latitude = mapSheet.Cells[i, 1].Value2;
                 string transitFees = Convert.ToString(mapSheet.Cells[i, 3].Value2);
-                string name = mapSheet.Cells[i,4].Value2;
+                string name = mapSheet.Cells[i, 4].Value2;
                 City city = new City(name, longitude, latitude, transitFees);
-                cityList.Add(city);
                 map.AddVertex(city);
                 int j = 5;
                 while (mapSheet.Cells[i, j + 1].Value2 != null)
@@ -57,7 +114,7 @@ namespace TravelAgency
                     if (Convert.ToString(mapSheet.Cells[i, j].Value2) != "∞")
                     {
                         int distance = Convert.ToInt32(mapSheet.Cells[i, j].Value2);
-                        map.AddEdge(new TaggedUndirectedEdge<City, int>(cityList[i - 2], cityList[j - 5], distance));
+                        map.AddEdge(i - 2, j - 5, distance);
                     }
                     j++;
                 }
